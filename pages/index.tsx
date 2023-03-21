dfa08861fc3694016b5508657ff47c37cdb8932f
lubwama <bizzicole87@gmail.com>
2023-03-21 23:33:46 +0300

    Video collection

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ type HomePageProps = {
 const Home: NextPage<HomePageProps> = ({ courses }) => {
   return (
     <>
-      {courses.length > 0 ? (<Heading>View these video courses</Heading>) : (<Heading>There are no videos to view</Heading>)}
+      {courses.length > 0 ? (<Heading>View Our Video Collection </Heading>) : (<Heading>There are no videos to view</Heading>)}
       {courses.find(course => course.published === false) && (
         <Heading as="h4">Draft movies are only visible to you</Heading>
       )}
